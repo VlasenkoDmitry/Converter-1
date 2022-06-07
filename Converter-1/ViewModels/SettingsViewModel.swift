8fc31db5048b7fffef29740105a4525b8fa3f19d
Dmitry Vlasenko <vlasenko.dmitry1@gmail.com>
2022-06-07 10:52:55 +0300

    x.4.1 Creating files of a SettingsViewController and its model

---
+++ Converter-1/ViewModels/SettingsViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsViewModel.swift
+//  Converter-1
+//
+//  Created by Ap on 6.06.22.
+//
+
+import Foundation
+
+class SettingsViewModel {
+    
+    var arraySettings: [String] {
+        []
+    }
+    
+    
+    
+}
---
+++ Converter-1/Views/SettingsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsViewController.swift
+//  Converter-1
+//
+//  Created by Ap on 6.06.22.
+//
+
+import Foundation
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    var viewModel: SettingsViewModel?
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+}
+
